46c03667bb19f8cdb1b935c87d68bc840f6fe8d8
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-11-22 10:48:57 +0100

    Add unit tests

--- tests/request-error-test.ts
+++ tests/request-error-test.ts
@@ -2,6 +2,184 @@
 
 "use strict";
 
-describe("Request Error Handler", () => {
+import assert from "assert";
+import { RequestErrorHandler, RequestError } from "../src";
 
+describe("Request Error Handler", () => {
+    it("Should be able to handle a properly formatted error", () => {
+        let errorBody: string;
+        let requestError: RequestError;
+
+        errorBody = JSON.stringify({ code: "EXAMPLE_ERROR" });
+        requestError = { status: 404, body: errorBody };
+
+        let ok: boolean;
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "*", () => {
+                ok = true;
+            })
+            .add(403, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add("*", "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "EXAMPLE_ERROR", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "*", () => {
+                ok = true;
+            })
+            .add(403, "EXAMPLE_ERROR", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add("*", "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "EXAMPLE_ERROR", () => {
+                ok = true;
+            })
+            .add(404, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(403, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add("*", "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+    });
+
+    it("Should be able to handle a badly formatted JSON error", () => {
+        let errorBody: string;
+        let requestError: RequestError;
+
+        errorBody = JSON.stringify({ randomField: 1 });
+        requestError = { status: 404, body: errorBody };
+
+        let ok: boolean;
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "*", () => {
+                ok = true;
+            })
+            .add(403, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add("*", "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "EXAMPLE_ERROR", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "*", () => {
+                ok = true;
+            })
+            .add(403, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add("*", "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+    });
+
+    it("Should be able to handle a badly formatted non-JSON error", () => {
+        let errorBody: string;
+        let requestError: RequestError;
+
+        errorBody = "example-non-json";
+        requestError = { status: 403, body: errorBody };
+
+        let ok: boolean;
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(403, "*", () => {
+                ok = true;
+            })
+            .add("*", "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+    });
+
+    it("Should be able to handle an empty error", () => {
+        let requestError: RequestError;
+
+        requestError = { status: 0, body: "" };
+
+        let ok: boolean;
+
+        ok = false;
+
+        new RequestErrorHandler()
+            .add(400, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(404, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add(403, "*", () => {
+                assert.fail("Wrong handler branch");
+            })
+            .add("*", "*", () => {
+                ok = true;
+            })
+            .handle(requestError);
+
+        assert.equal(ok, true, "Expected handler branch was not called");
+    });
 });
--- tests/request-test.ts
+++ tests/request-test.ts
@@ -2,9 +2,232 @@
 
 "use strict";
 
-// Use emulated XMLHttpRequest
-global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+import { makeApiRequest } from "../src";
+import assert from "assert";
+import HTTP from "http";
+
+let port = 0;
+let testServer = "127.0.0.1";
+
+const LastRequest = {
+    method: "",
+    url: "",
+    headers: {} as any,
+    jsonBody: null as any,
+};
+
+const MustReturn = {
+    status: 200,
+    body: {} as any,
+};
+
+let server: HTTP.Server;
 
 describe("Request", () => {
+    before((done) => {
+        server = HTTP.createServer((req, res) => {
+            LastRequest.method = req.method || "";
+            LastRequest.url = req.url || "";
+            LastRequest.headers = req.headers;
+            LastRequest.jsonBody = null;
 
+            // console.log(`Received request: ${LastRequest.method} ${LastRequest.url}`);
+
+            let data = "";
+
+            req.on("data", chunk => {
+                data += chunk;
+            });
+
+            req.on("end", () => {
+                try {
+                    LastRequest.jsonBody = JSON.parse(data);
+                } catch (ex) { }
+
+                res.writeHead(MustReturn.status, { "Content-Type": "application/json" });
+                res.write(JSON.stringify(MustReturn.body));
+                res.end();
+            });
+        }).listen(0, "127.0.0.1", undefined, () => {
+            port = (server.address() as any).port;
+            testServer = "127.0.0.1:" + port;
+            // console.log("Test server listening on " + testServer);
+            done();
+        });
+    });
+
+    it("Should be able to perform a GET request", async () => {
+        return new Promise((resolve, reject) => {
+            MustReturn.status = 200;
+            MustReturn.body = { field: "example" };
+
+            makeApiRequest({
+                method: "GET",
+                url: `http://${testServer}/api/test/get?a=1&b=2`,
+            })
+                .onSuccess((res) => {
+                    try {
+                        assert.equal(res.field, "example");
+                        assert.equal(LastRequest.method, "GET");
+                        assert.equal(LastRequest.url, "/api/test/get?a=1&b=2");
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                    resolve();
+                })
+                .onRequestError((err) => {
+                    reject(new Error("Request error: " + err.status))
+                })
+                .onUnexpectedError((err) => {
+                    reject(err);
+                });
+        });
+    });
+
+    it("Should be able to handle a GET request error", async () => {
+        return new Promise((resolve, reject) => {
+            MustReturn.status = 404;
+            MustReturn.body = { code: "NOT_FOUND" };
+
+            makeApiRequest({
+                method: "GET",
+                url: `http://${testServer}/api/test/get?a=1&b=2`,
+                headers: {
+                    "example-header": "example-value",
+                },
+            })
+                .onSuccess(() => {
+                    reject(new Error("Expected request error but got success"));
+                })
+                .onRequestError((err) => {
+                    try {
+                        assert.equal(err.status, 404);
+                        assert.deepEqual(JSON.parse(err.body), { code: "NOT_FOUND" });
+                        assert.equal(LastRequest.method, "GET");
+                        assert.equal(LastRequest.url, "/api/test/get?a=1&b=2");
+                        assert.equal(LastRequest.headers["example-header"], "example-value");
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                    resolve();
+                })
+                .onUnexpectedError((err) => {
+                    reject(err);
+                });
+        });
+    });
+
+    it("Should be able to perform a POST request", async () => {
+        return new Promise((resolve, reject) => {
+            MustReturn.status = 200;
+            MustReturn.body = { field: "example" };
+
+            const jsonBody = {
+                bodyField1: 10,
+                bodyField2: "test",
+            };
+
+            makeApiRequest({
+                method: "POST",
+                url: `http://${testServer}/api/test/post`,
+                json: jsonBody,
+                headers: {
+                    "example-header": "example-value",
+                },
+            })
+                .onSuccess((res) => {
+                    try {
+                        assert.equal(res.field, "example");
+                        assert.equal(LastRequest.method, "POST");
+                        assert.equal(LastRequest.url, "/api/test/post");
+                        assert.equal(LastRequest.headers["example-header"], "example-value");
+                        assert.deepEqual(LastRequest.jsonBody, jsonBody);
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                    resolve();
+                })
+                .onRequestError((err) => {
+                    reject(new Error("Request error: " + err.status))
+                })
+                .onUnexpectedError((err) => {
+                    reject(err);
+                });
+        });
+    });
+
+    it("Should be able to perform a PUT request", async () => {
+        return new Promise((resolve, reject) => {
+            MustReturn.status = 200;
+            MustReturn.body = { field: "example" };
+
+            const jsonBody = {
+                bodyField1: 10,
+                bodyField2: "test",
+            };
+
+            makeApiRequest({
+                method: "PUT",
+                url: `http://${testServer}/api/test/put`,
+                json: jsonBody,
+                headers: {
+                    "example-header": "example-value",
+                },
+            })
+                .onSuccess((res) => {
+                    try {
+                        assert.equal(res.field, "example");
+                        assert.equal(LastRequest.method, "PUT");
+                        assert.equal(LastRequest.url, "/api/test/put");
+                        assert.equal(LastRequest.headers["example-header"], "example-value");
+                        assert.deepEqual(LastRequest.jsonBody, jsonBody);
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                    resolve();
+                })
+                .onRequestError((err) => {
+                    reject(new Error("Request error: " + err.status))
+                })
+                .onUnexpectedError((err) => {
+                    reject(err);
+                });
+        });
+    });
+
+    it("Should be able to perform a DELETE request", async () => {
+        return new Promise((resolve, reject) => {
+            MustReturn.status = 200;
+            MustReturn.body = { field: "example" };
+
+            makeApiRequest({
+                method: "DELETE",
+                url: `http://${testServer}/api/test/delete?a=1&b=2`,
+                headers: {
+                    "example-header": "example-value",
+                },
+            })
+                .onSuccess((res) => {
+                    try {
+                        assert.equal(res.field, "example");
+                        assert.equal(LastRequest.method, "DELETE");
+                        assert.equal(LastRequest.url, "/api/test/delete?a=1&b=2");
+                        assert.equal(LastRequest.headers["example-header"], "example-value");
+                    } catch (ex) {
+                        reject(ex);
+                    }
+                    resolve();
+                })
+                .onRequestError((err) => {
+                    reject(new Error("Request error: " + err.status))
+                })
+                .onUnexpectedError((err) => {
+                    reject(err);
+                });
+        });
+    });
+
+    after(() => {
+        server.close();
+    });
 });
